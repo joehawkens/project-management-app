2ac7a97028d45c5f5b3ec783165297e7a5d4ae63
Joe Hawkins <hawkinsjwh@gmail.com>
2022-07-08 12:09:39 -0600

    added oauth library

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 console.log("SERVER IS RUNNING.");
 
-import axios from 'axios';
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'secret',
+  baseURL: 'https://joehawkens.github.io/index.html',
+  clientID: 'hynsEG2zks4qUJ0Et8MN1Azt1r9NIEn5',
+  issuerBaseURL: 'https://dev-gc9an4sn.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
