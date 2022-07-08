df3ba27de755c24c8db6811ae9672d826a59eae3
Joe Hawkins <hawkinsjwh@gmail.com>
2022-07-08 12:33:52 -0600

    attempting to fix

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 console.log("SERVER IS RUNNING.");
 
+
+const express = require('express');
 const { auth } = require('express-openid-connect');
+const app = express()
+
+
 
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'secret',
+  secret: 'a long, randomly-generated string stored in env',
   baseURL: 'https://joehawkens.github.io/project-management-app/index.html',
   clientID: 'hynsEG2zks4qUJ0Et8MN1Azt1r9NIEn5',
   issuerBaseURL: 'https://dev-gc9an4sn.us.auth0.com'
