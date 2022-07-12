e8f6734a1d88db26456e98f3a00e9e57f5058f30
Joe Hawkins <hawkinsjwh@gmail.com>
2022-07-12 00:13:00 -0600

    more changes

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-console.log("JAVSCRIPT IS CONNECTED.");
+// REGISTRATION FORM DATA 
+
+const registrationForm = document.querySelector('#register-form');
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    // turns data into JSON...
+
+    const data = new FormData(event.target);
+    const username = data.get('username');
+    const password = data.get('password');
+    console.log({ username, password });
+
+    // POSTS JSON into local Database...
+
+    
 
 
-import DataService from '/DataService.js';
+  }
+  
 
-
+registrationForm.addEventListener('submit', handleSubmit);
+  
