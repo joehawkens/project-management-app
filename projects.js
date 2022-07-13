abb22aa79bda19bcc35d3f4414820e720ed92ffd
Joe Hawkins <hawkinsjwh@gmail.com>
2022-07-13 12:25:46 -0600

    added dynamically generated add project form

--- projects.js
+++ projects.js
@@ -13,4 +13,40 @@ function logout() {
 document.querySelector("#logout").addEventListener("click", logout)
 
 
-//
+
+
+// Add Project Button - when pressed, will dynamically generate a form
+// to fill with project info (title, description, etc.)
+
+
+
+const addProjectButton = document.querySelector("#add-project-button")
+
+
+
+function showProjects() {
+    
+    const projectsDiv = document.querySelector('#projects');
+    projectsDiv.innerHTML = 
+    `
+    <label for="Project Title">Project Title</label>
+    <input type="text" id="project-title" name="project-title" form="add-project">
+    <label for="project-description">Project Description</label>
+    <textarea name="project-description" resize="none" id="project-description" form="add-project" cols="30" rows="10"></textarea>
+    <form id="add-project">
+        <input type="submit" value="Create Project">
+    </form>
+    
+    `
+    document.querySelector("#add-project-button").style.display = "none";
+    
+}
+
+
+
+
+
+
+
+
+addProjectButton.addEventListener("click", showProjects)
